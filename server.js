e2553d585575dfdebf8ffaec3a6c4c053c588a60
Ella Mae Freshwater <emfresh2o@Ellas-MacBook-Pro.local>
2020-11-07 15:28:21 -0500

    created routers

--- server.js
+++ server.js
@@ -4,10 +4,21 @@ const helmet = require('helmet');
 
 const server = express();
 
+const actionRouter = require('./data/routers/projectRouter.js');
+const projectRouter = require('.routers/actionRouter.js');
+
 server.use(express.json());
 server.use(helmet());
+server.use((error, req, res, next) => {
+  console.log(err);
+  res.status(error.code).json({ error });
+});
 
 server.get('/', (req, res) => {
-  res.status(200).json({ message: 'Server is online!' });
+  res.status(200).json({ message: 'The Server is LIVE!' });
 });
+
+server.use('/api/action', actionRouter);
+server.use('/api/project', projectRouter);
+
 module.exports = server;
